aab6ef3978d0603a246aaa7c0cddde92bec2ec03
usr109283jsmzs812 <83765120+usr149928321@users.noreply.github.com>
2022-05-06 16:05:10 +0500

    ??

--- next.config.cjs
+++ next.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
+    BASE_URL: "https://tages-mongo-next.herokuapp.com",
     MONGO_URI:
       "mongodb+srv://test:test@cluster0.ugvtp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   },
